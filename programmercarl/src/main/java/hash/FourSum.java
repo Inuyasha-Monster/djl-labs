897afb2aed79b31aa01a9b908e8d9ef47180adcb
djl <djl.du@huolala.cn>
2022-05-30 20:19:39 +0800

    4数之和

---
+++ programmercarl/src/main/java/hash/FourSum.java
@@ -0,0 +1,57 @@
+package hash;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author djl
+ */
+public class FourSum {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        if (nums.length < 4) {
+            return new ArrayList<>();
+        }
+        Arrays.sort(nums);
+        List<List<Integer>> arr = new ArrayList<>();
+        for (int i = 0; i < nums.length - 3; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int l = j + 1;
+                int r = nums.length - 1;
+                while (l < r) {
+                    int sum = nums[i] + nums[j] + nums[l] + nums[r];
+                    if (sum == target) {
+                        //存入
+                        arr.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[l], nums[r])));
+                        //去重
+                        while (l < r && nums[l] == nums[l + 1]) {
+                            l++;
+                        }
+                        while (l < r && nums[r] == nums[r - 1]) {
+                            r--;
+                        }
+                        l++;
+                        r--;
+                    } else if (sum > target) {
+                        r--;
+                    } else {
+                        l++;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
