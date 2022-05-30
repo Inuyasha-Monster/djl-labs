e710e1729ae6073c9c97c2975363a0a77ec289f0
djl <djl.du@huolala.cn>
2022-05-30 20:18:38 +0800

    采用map

--- programmercarl/src/main/java/hash/ThreeSum.java
+++ programmercarl/src/main/java/hash/ThreeSum.java
@@ -78,6 +78,46 @@ public class ThreeSum {
         return res;
     }
 
+    public List<List<Integer>> threeSum3(int[] nums) {
+        if (nums.length < 3) {
+            return new ArrayList<>();
+        }
+        //排序
+        Arrays.sort(nums);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<List<Integer>> resultarr = new ArrayList<>();
+        //存入哈希表
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        Integer t;
+        int target = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            target = -nums[i];
+            //去重
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length; ++j) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                if ((t = map.get(target - nums[j])) != null) {
+                    //符合要求的情况,存入
+                    if (t > j) {
+                        resultarr.add(new ArrayList<>
+                                (Arrays.asList(nums[i], nums[j], nums[t])));
+
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return resultarr;
+    }
+
+
     public static void main(String[] args) {
         List<Integer> list1 = new ArrayList<>();
         list1.add(1);
