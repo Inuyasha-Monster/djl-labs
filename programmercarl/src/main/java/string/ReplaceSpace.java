c328cc6bc1cba2f08263d78f9ffc3bb9d6516ca3
djl <djl.du@huolala.cn>
2022-05-31 14:29:16 +0800

    计算扩容所需长度进行定长处理

--- programmercarl/src/main/java/string/ReplaceSpace.java
+++ programmercarl/src/main/java/string/ReplaceSpace.java
@@ -17,8 +17,41 @@ public class ReplaceSpace {
         return sb.toString();
     }
 
+    /**
+     * 使用静态数组的方式，防止扩容
+     *
+     * @param s
+     * @return
+     */
+    public static String replaceSpace2(String s) {
+        final char[] chars = s.toCharArray();
+        // 统计空格的次数
+        int count = 0;
+        for (char c : chars) {
+            if (c == ' ') {
+                count++;
+            }
+        }
+        // 申请新的char数组
+        char[] res = new char[chars.length + count * 2];
+        int index = 0;
+        for (char c : chars) {
+            if (c == ' ') {
+                res[index] = '%';
+                index++;
+                res[index] = '2';
+                index++;
+                res[index] = '0';
+            } else {
+                res[index] = c;
+            }
+            index++;
+        }
+        return new String(res);
+    }
+
     public static void main(String[] args) {
-        final String replaceSpace = replaceSpace("We are happy.");
+        final String replaceSpace = replaceSpace2("We are happy.");
         System.out.println("replaceSpace = " + replaceSpace);
     }
 }
