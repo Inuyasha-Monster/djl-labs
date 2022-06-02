f0ad749a046f1a9fb8f18d1b1d8c34b0ecbb43a5
djl <djl.du@huolala.cn>
2022-06-02 15:53:16 +0800

    寻找子串增加注释

--- programmercarl/src/main/java/string/StrStr.java
+++ programmercarl/src/main/java/string/StrStr.java
@@ -23,17 +23,19 @@ public class StrStr {
             return 0;
         }
         int n = haystack.length();
+        // 如果目标字符串长度大于原始字符串长度直接返回
         if (n < m) {
             return -1;
         }
         int i = 0;
         int j = 0;
         while (i < n - m + 1) {
-            // 找到首字母相等
+            // 找到2个串的首字母相等，i移动，j不动
             while (i < n && haystack.charAt(i) != needle.charAt(j)) {
                 i++;
             }
-            if (i == n) {// 没有首字母相等的
+            // 如果i遍历到目标末尾了，还没有首字母相等的
+            if (i == n) {
                 return -1;
             }
             // 遍历后续字符，判断是否相等
@@ -43,11 +45,11 @@ public class StrStr {
                 i++;
                 j++;
             }
-            // 找到
+            // 找到结果
             if (j == m) {
                 return i - j;
             } else {
-                // 未找到
+                // 未找到，回拨i的指针
                 i -= j - 1;
                 j = 0;
             }
