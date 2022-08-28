7a4bc3d0a49e44eafc3587dce1aa869970e6e660
djl <djl.du@huolala.cn>
2022-08-28 17:29:32 +0800

    研究restTemplate是如何整合httpClient的

--- http-client-test/spring-boot-test/src/main/java/com/djl/config/MyConfig.java
+++ http-client-test/spring-boot-test/src/main/java/com/djl/config/MyConfig.java
@@ -1,16 +1,31 @@
 package com.djl.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 /**
  * @author djl
  */
 @Configuration
 public class MyConfig {
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean(name = "httpClient")
+    public RestTemplate httpClientRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectionRequestTimeout(2000);
+        return restTemplateBuilder.requestFactory(() -> factory)
+                .setConnectTimeout(Duration.ofMillis(2000))
+                .setReadTimeout(Duration.ofMillis(3000))
+                .build();
     }
 }
--- http-client-test/spring-boot-test/src/main/java/com/djl/controller/TestController.java
+++ http-client-test/spring-boot-test/src/main/java/com/djl/controller/TestController.java
@@ -4,6 +4,8 @@ import org.apache.http.HttpStatus;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.config.ConnectionConfig;
+import org.apache.http.config.SocketConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author djl
@@ -24,7 +27,12 @@ public class TestController {
     private final static CloseableHttpClient CLIENT;
 
     static {
-        CLIENT = HttpClients.custom().setMaxConnPerRoute(10).setMaxConnTotal(100).build();
+        CLIENT = HttpClients.custom()
+                .setConnectionManagerShared(false)
+                .setDefaultConnectionConfig(ConnectionConfig.custom().setCharset(StandardCharsets.UTF_8).build())
+                .setDefaultRequestConfig(RequestConfig.custom().setSocketTimeout(3000).setConnectTimeout(2000).setConnectionRequestTimeout(2000).build())
+                .setDefaultSocketConfig(SocketConfig.custom().setTcpNoDelay(true).build())
+                .setMaxConnPerRoute(10).setMaxConnTotal(100).build();
     }
 
     private final RestTemplate restTemplate;
@@ -43,7 +51,10 @@ public class TestController {
         CloseableHttpResponse httpResponse = null;
         String str = "error";
         try {
-            final RequestConfig config = RequestConfig.custom().setConnectionRequestTimeout(2000).build();
+            final RequestConfig config = RequestConfig.custom()
+                    .setConnectionRequestTimeout(2000)
+                    .setConnectTimeout(2000)
+                    .setSocketTimeout(3000).build();
             final HttpGet httpGet = new HttpGet("http://localhost:8080/test/hello");
             httpGet.setConfig(config);
 
