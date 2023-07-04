493338da95755031d2813aff1e5eaf08aa8fced3
djl <djl.du@huolala.cn>
2023-07-04 17:47:35 +0800

    给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。

--- programmercarl/src/main/java/other/LeetCode_20230704.java
+++ programmercarl/src/main/java/other/LeetCode_20230704.java
@@ -2,7 +2,8 @@ package other;
 
 import tree.TreeNode;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class LeetCode_20230704 {
 
@@ -135,6 +136,93 @@ public class LeetCode_20230704 {
 
         // 如果左右路径都不符合则返回匹配失败
         return false;
+    }
+
+    /**
+     * 广度遍历优先
+     *
+     * @param root
+     * @param targetSum
+     * @return
+     */
+    public boolean hasPathSum2(TreeNode root, int targetSum) {
+        // 核心思路：利用队列模拟树的遍历
+
+        if (root == null) {
+            return false;
+        }
+
+        Queue<TreeNode> q_node = new LinkedList<>();
+        Queue<Integer> q_path_sum = new LinkedList<>();
+
+        q_node.offer(root);
+        q_path_sum.offer(root.val);
+
+        while (!q_node.isEmpty()) {
+            final TreeNode node = q_node.poll();
+            final Integer v = q_path_sum.poll();
+            if (node.left == null && node.right == null && v == targetSum) {
+                return true;
+            }
+
+            if (node.left != null) {
+                q_node.offer(node.left);
+                q_path_sum.offer(v + node.left.val);
+            }
+
+            if (node.right != null) {
+                q_node.offer(node.right);
+                q_path_sum.offer(v + node.right.val);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
+     * <p>
+     * 叶子节点 是指没有子节点的节点。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode.cn/problems/path-sum-ii
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param root
+     * @param targetSum
+     * @return
+     */
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        // 变种：这里是找出所有符合要求的路径
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        // 思路 1：直接找出所有的路径，然后再判断过滤
+        List<List<Integer>> result = new ArrayList<>();
+        innerImpl2(root, new ArrayList<>(), result);
+        return result.stream().filter(x -> x.stream().mapToInt(c -> c).sum() == targetSum).collect(Collectors.toList());
+    }
+
+    private void innerImpl2(TreeNode node, List<Integer> nodePath, List<List<Integer>> result) {
+        // 大方向：中 左 右
+
+        nodePath.add(node.val);
+
+        // 叶子节点
+        if (node.left == null && node.right == null) {
+            result.add(new ArrayList<>(nodePath));
+        }
+
+        if (node.left != null) {
+            innerImpl2(node.left, nodePath, result);
+            // 回溯
+            nodePath.remove(nodePath.size() - 1);
+        }
+
+        if (node.right != null) {
+            innerImpl2(node.right, nodePath, result);
+            nodePath.remove(nodePath.size() - 1);
+        }
     }
 
     public static void main(String[] args) {
