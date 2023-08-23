7acad09e8e03ea1f52edaaef3f52062cecb6d04a
djl <djl.du@huolala.cn>
2023-08-23 16:01:16 +0800

    尝试groovy动态脚本语言

--- groovy-study/src/main/java/com/djl/App1.java
+++ groovy-study/src/main/java/com/djl/App1.java
@@ -13,27 +13,19 @@ public class App1 {
 
         String mapScript = "import com.djl.User\n" +
                 "\n" +
-                "import java.util.function.Consumer\n" +
                 "import java.util.function.Predicate\n" +
                 "import java.util.stream.Collectors\n" +
                 "\n" +
-                "// 通过java将传输参数封装为map的格式传递到脚本中\n" +
-                "List<User> handle(List<User> list, int a) {\n" +
+                "def handle(List<User> list, Integer age) {\n" +
                 "    if (list == null || list.isEmpty()) {\n" +
                 "        return Collections.emptyList();\n" +
                 "    }\n" +
                 "    def result = list.stream().filter(new Predicate<User>() {\n" +
                 "        @Override\n" +
                 "        boolean test(User user) {\n" +
-                "            return user.getAge() > 18;\n" +
+                "            return user.getAge() > age;\n" +
                 "        }\n" +
                 "    }).collect(Collectors.toList())\n" +
-                "    result.forEach(new Consumer<User>() {\n" +
-                "        @Override\n" +
-                "        void accept(User user) {\n" +
-                "            user.setAge(user.getAge() + a);\n" +
-                "        }\n" +
-                "    })\n" +
                 "    return result;\n" +
                 "}";
 
@@ -46,7 +38,7 @@ public class App1 {
 
         List<Object> objects = new ArrayList<>();
         objects.add(users);
-        objects.add(2);
+        objects.add(25);
 
         final Object handleResult = ((GroovyObject) mapClass.newInstance()).invokeMethod("handle", objects);
         @SuppressWarnings("unchecked") final List<User> list = (List<User>) handleResult;
---
+++ groovy-study/src/main/java/com/djl/Test2.groovy
@@ -0,0 +1,17 @@
+import com.djl.User
+
+import java.util.function.Predicate
+import java.util.stream.Collectors
+
+def handle(List<User> list, Integer age) {
+    if (list == null || list.isEmpty()) {
+        return Collections.emptyList();
+    }
+    def result = list.stream().filter(new Predicate<User>() {
+        @Override
+        boolean test(User user) {
+            return user.getAge() > age;
+        }
+    }).collect(Collectors.toList())
+    return result;
+}
